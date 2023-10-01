e746594252f650995413cce00957822f3a27b64c
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-10-01 18:26:46 +0530

    We doing Network calls and pending, success and error handling response the right way and showing notification at the top of navbar, now work done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+import { showNotification } from './components/Redux/uiSlice';
+import Notification from './components/UI/Notification';
+
+
+let isInitial = true;
 
 function App() {
 
-  const { cartIsVisible } = useSelector(state => state.ui)
+  const dispatch = useDispatch();
+  const { cartIsVisible, notification } = useSelector(state => state.ui)
+  const { cartItems } = useSelector(state => state.cart)
+
+  // console.log("Inside App Component Notification -->", notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+      );
+
+      const response = await fetch(
+        'https://reduxtoolkitcart-c3fbf-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartItems),
+        }
+      );
+      // console.log("Inside App Component useEffect() to check response -->", response.ok)
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!',
+      })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      })
+      );
+    });
+  }, [cartItems, dispatch]);
+
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (<Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message} />)}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/Redux/uiSlice.jsx
+++ src/components/Redux/uiSlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialUiState = {
     cartIsVisible: false,
+    notification: null,
 };
 
 const uiSlice = createSlice({
@@ -11,8 +12,16 @@ const uiSlice = createSlice({
         toggleCartAction(state) {
             state.cartIsVisible = !state.cartIsVisible;
         },
+        showNotification(state, actions) {
+            console.log("Inside Redux Action -->", actions.payload);
+            state.notification = {
+                status: actions.payload.status,
+                title: actions.payload.title,
+                message: actions.payload.message,
+            };
+        },
     },
 });
 
-export const { toggleCartAction } = uiSlice.actions;
+export const { toggleCartAction, showNotification } = uiSlice.actions;
 export default uiSlice.reducer;
